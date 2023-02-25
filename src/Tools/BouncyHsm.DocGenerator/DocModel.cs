475edad230ad17c159219de194ea6ea68b7add8f
harrison314 <harrison314@azet.sk>
2023-02-25 09:14:20 +0100

    Add BouncyHsm.DocGenerator

---
+++ src/Tools/BouncyHsm.DocGenerator/DocModel.cs
@@ -0,0 +1,32 @@
+﻿using BouncyHsm.Core.Services.Contracts.Entities;
+using BouncyHsm.Core.UseCases.Contracts;
+
+namespace BouncyHsm.DocGenerator;
+
+public class DocModel
+{
+    public List<MechanismInfoData> Mechanisms
+    {
+        get;
+        internal set;
+    }
+
+    public List<SupportedNameCurve> Ec
+    {
+        get;
+        internal set;
+    }
+
+    public BouncyHsmVersion Versions
+    {
+        get;
+        internal set;
+    }
+
+    public DocModel()
+    {
+        this.Mechanisms = new List<MechanismInfoData>();
+        this.Ec = new List<SupportedNameCurve>();
+        this.Versions = new BouncyHsmVersion("", "", "", "");
+    }
+}
---
+++ src/Tools/BouncyHsm.DocGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using BouncyHsm.Core.UseCases.Implementation;
+using Scriban;
+
+namespace BouncyHsm.DocGenerator;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        string ouputPath = "Doc.md";
+        if (args.Length > 0)
+        {
+            ouputPath = args[0];
+        }
+
+        Template template = Template.Parse(File.ReadAllText("Algorithms.txt"));
+        DocModel model = CreateModel();
+#pragma warning disable CS8603 // Possible null reference return.
+        string content = template.Render(model, new Scriban.Runtime.MemberRenamerDelegate(t => t?.Name));
+#pragma warning restore CS8603 // Possible null reference return.
+
+        File.WriteAllText(ouputPath, content, System.Text.Encoding.UTF8);
+    }
+
+    private static DocModel CreateModel()
+    {
+        HsmInfoFacade infoFacade = new HsmInfoFacade();
+        DocModel model = new DocModel();
+
+        model.Versions = infoFacade.GetVersions();
+        model.Mechanisms = infoFacade.GetAllMechanism().ToList();
+        model.Ec = infoFacade.GetCurves().ToList();
+
+        return model;
+    }
+}
