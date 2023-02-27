4bd2de8743161cb28441e0e5abf14c7f38312f37
harrison314 <harrison314@azet.sk>
2023-02-27 19:22:55 +0100

    Create base NUKE build.

---
+++ build/Build.Native.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Build.Tasks;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tools.MSBuild;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using System.IO.Compression;
+using System.IO;
+
+public partial class Build
+{
+    Target BuildPkcs11LibWin32 => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            BuildBouncyHsmPkcs11Lib(MSBuildTargetPlatform.Win32);
+            AbsolutePath nativeLib = SourceDirectory / "BouncyHsm.Pkcs11Lib" / Configuration / "BouncyHsm.Pkcs11Lib.dll";
+            AbsolutePath destination = ArtifactsTmpDirectory / "native" / "Win-x86";
+            EnsureCleanDirectory(destination);
+            CopyFileToDirectory(nativeLib, destination);
+        });
+
+    Target BuildPkcs11LibX64 => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            BuildBouncyHsmPkcs11Lib(MSBuildTargetPlatform.x64);
+            AbsolutePath nativeLib = SourceDirectory / "BouncyHsm.Pkcs11Lib" / "x64" / Configuration / "BouncyHsm.Pkcs11Lib.dll";
+            AbsolutePath destination = ArtifactsTmpDirectory / "native" / "Win-x64";
+            EnsureCleanDirectory(destination);
+            CopyFileToDirectory(nativeLib, destination);
+        });
+
+    private void BuildBouncyHsmPkcs11Lib(MSBuildTargetPlatform platform)
+    {
+        AbsolutePath p11Proj = SourceDirectory / "BouncyHsm.Pkcs11Lib" / "BouncyHsm.Pkcs11Lib.vcxproj";
+        MSBuild(_ => _
+           .SetTargetPath(p11Proj)
+           .SetConfiguration(Configuration)
+           .SetTargetPlatform(platform)
+           .SetTargets("clean", "build"));
+    }
+}
---
+++ build/Build.cs
@@ -0,0 +1,107 @@
+using System;
+using System.IO.Compression;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Tasks;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using System.Text;
+using Serilog;
+
+public partial class Build : NukeBuild
+{
+    public static int Main() => Execute<Build>(x => x.BuildAll);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [GitRepository]
+    readonly GitRepository Repository;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src" / "Src";
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath ArtifactsTmpDirectory => RootDirectory / "artifacts" / ".tmp";
+
+
+    Target Clean => _ => _
+        .Executes(() =>
+        {
+            GlobDirectories(SourceDirectory, "**/bin", "**/obj").ForEach(DeleteDirectory);
+            //TODO: delete native directories
+
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
+
+    Target BuildBouncyHsm => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            AbsolutePath projectFile = SourceDirectory / "BouncyHsm" / "BouncyHsm.csproj";
+            AbsolutePath outputDir = ArtifactsTmpDirectory / "BouncyHsm";
+
+            DotNetPublish(_ => _
+            .SetConfiguration(Configuration)
+            .AddProperty("GitCommit", Repository.Commit)
+            .SetProject(projectFile)
+            .SetOutput(outputDir));
+        });
+
+    Target BuildAll => _ => _
+        .DependsOn(Clean)
+        .DependsOn(BuildPkcs11LibWin32)
+        .DependsOn(BuildPkcs11LibX64)
+        .DependsOn(BuildBouncyHsm)
+        .Executes(() =>
+        {
+            CopyDirectoryRecursively(ArtifactsTmpDirectory / "native", ArtifactsTmpDirectory / "BouncyHsm" / "native");
+            CreateZip(ArtifactsTmpDirectory / "native" / "Win-x64" / "BouncyHsm.Pkcs11Lib.dll",
+                "Win X64",
+                "0.1.0.0", //TODO
+                ArtifactsTmpDirectory / "BouncyHsm" / "wwwroot" / "native" / "BouncyHsm.Pkcs11Lib-Winx64.zip");
+            CreateZip(ArtifactsTmpDirectory / "native" / "Win-x86" / "BouncyHsm.Pkcs11Lib.dll",
+                "Win X86",
+                "0.1.0.0", //TODO
+                ArtifactsTmpDirectory / "BouncyHsm" / "wwwroot" / "native" / "BouncyHsm.Pkcs11Lib-Winx86.zip");
+
+            Nuke.Common.IO.CompressionTasks.CompressZip(ArtifactsTmpDirectory / "BouncyHsm",
+                ArtifactsDirectory / "BouncyHsm.zip",
+                t => t.Extension != ".pdb" && t.Name != "libman.json" && t.Name != ".gitkeep");
+        });
+
+
+    private void CreateZip(AbsolutePath dllFile, string platform, string version, AbsolutePath destination)
+    {
+        Log.Debug("Creating ZIP file from dll {0}", dllFile);
+
+        using FileStream fs = new FileStream(destination, FileMode.Create);
+        using ZipArchive archive = new ZipArchive(fs, ZipArchiveMode.Create);
+        archive.CreateEntryFromFile(dllFile, dllFile.Name, CompressionLevel.Optimal);
+        ZipArchiveEntry zipArchiveEntry = archive.CreateEntry("Readme.txt");
+        using Stream readmeStream = zipArchiveEntry.Open();
+
+        byte[] content = Encoding.UTF8.GetBytes(@$"Bouncy Hsm PKCS11 library
+
+Version: {version}
+For platform: {platform}
+Git commit: {Repository.Commit}
+
+Project site: https://github.com/harrison314/BouncyHsm
+License: BSD 3 Clausule
+");
+
+        readmeStream.Write(content);
+        readmeStream.Flush();
+    }
+
+}
