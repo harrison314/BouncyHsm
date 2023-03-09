da92d9a6d66bbf9a5ffb1991f37fb27e327373cd
Jozef Gajdos <jozef.gajdos@disig.sk>
2023-03-09 09:36:13 +0100

    Add generate PKCS#10 request to SPA.

---
+++ src/Src/BouncyHsm.Spa/Pages/PkcsPages/GenerateCsrModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BouncyHsm.Spa.Pages.PkcsPages;
+
+public class GenerateCsrModel
+{
+    [Required]
+    [MinLength(3)]
+    public string Subject
+    {
+        get;
+        set;
+    }
+
+    public GenerateCsrModel()
+    {
+        this.Subject = string.Empty;
+    }
+}
---
+++ src/Src/BouncyHsm.Spa/Shared/JSRuntimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+
+namespace BouncyHsm.Spa.Shared;
+
+internal static class JSRuntimeExtensions
+{
+    public static async Task DownloadFile(this IJSRuntime js, string fileName, byte[] content)
+    {
+        using MemoryStream ms = new MemoryStream(content);
+        using DotNetStreamReference streamRef = new DotNetStreamReference(stream: ms);
+
+        await js.InvokeVoidAsync("downloadFileFromStream", fileName, streamRef);
+    }
+}
