be91ca0d96e8bdcc9cceede96f80fb608559799f
Jozef Gajdos <jozef.gajdos@disig.sk>
2023-03-29 14:16:25 +0200

    Fix CkaModulusBits in import key.

--- src/Src/BouncyHsm.Cli/Commands/Pkcs/ListPkcsObjectCommand.cs
+++ src/Src/BouncyHsm.Cli/Commands/Pkcs/ListPkcsObjectCommand.cs
@@ -4,6 +4,7 @@ using Spectre.Console;
 using Spectre.Console.Cli;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ internal class ListPkcsObjectCommand : AsyncCommand<ListPkcsObjectCommand.Settin
     internal sealed class Settings : BaseSettings
     {
         [CommandArgument(0, "[SlotId]")]
+        [Description("Slot Id.")]
         public int SlotId
         {
             get;
@@ -24,34 +26,42 @@ internal class ListPkcsObjectCommand : AsyncCommand<ListPkcsObjectCommand.Settin
 
     public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
     {
-        //BouncyHsmClient client = BouncyHsmClientFactory.Create(settings.Endpoint);
-        //PkcsObjectsDto objects = default!;
+        BouncyHsmClient client = BouncyHsmClientFactory.Create(settings.Endpoint);
+        PkcsObjectsDto objects = default!;
 
-        //await AnsiConsole.Status()
-        //    .StartAsync("Loading...", async ctx =>
-        //    {
-        //        objects = await client.GetPkcsObjectsAsync(settings.SlotId);
-        //    });
+        await AnsiConsole.Status()
+            .StartAsync("Loading...", async ctx =>
+            {
+                objects = await client.GetPkcsObjectsAsync(settings.SlotId);
+            });
 
-        //Table table = new Table();
-        //table.AddColumn("Id");
-        //table.AddColumn("Description");
-        //table.AddColumn("Token Label");
-        //table.AddColumn("Token SerialNumber");
-        //table.AddColumn("With HW RNG");
-        //table.AddColumn("With QualifiedArea");
+        foreach (PkcsObjectInfoDto info in objects.Objects)
+        {
+            AnsiConsole.MarkupLine("[green]{0}[/]", info.Subject ?? "-");
+            Grid grid = new Grid();
+            grid.AddColumn().AddColumn();
 
-        //foreach (PkcsObjectInfoDto info in objects.Objects)
-        //{
-        //    table.AddRow(new Markup($"[green]{}[/]"),
-        //        new Markup(Markup.Escape(slot.Description)),
-        //        new Markup(Markup.Escape(slot.Token!.Label)),
-        //        new Markup(Markup.Escape(slot.Token!.SerialNumber)),
-        //        new Markup(slot.Token!.SimulateHwRng ? "[green]yes[/]" : "[yellow]no[/]"),
-        //        new Markup(slot.Token!.SimulateQualifiedArea ? "[green]yes[/]" : "[yellow]no[/]"));
-        //}
+            grid.AddRow(new string[] { "CkaLabel:", Markup.Escape(info.CkaLabel) });
+            grid.AddRow(new string[] { "CkaId:", Markup.Escape(HexConvertorSlim.ToHex(info.CkaId)) });
+            grid.AddRow(new string[] { "Always Authenticate:", info.AlwaysAuthenticate? "[green]yes[/]" : "[yellow]no[/]" });
+            AnsiConsole.Write(grid);
 
-        //AnsiConsole.Write(table);
+            Table table = new Table();
+            table.AddColumn("Id");
+            table.AddColumn("CKO");
+            table.AddColumn("Description");
+
+            foreach (PkcsSpecificObjectDto specificObject in info.Objects)
+            {
+                table.AddRow(new Markup($"[green]{specificObject.ObjectId}[/]"),
+                    new Markup(specificObject.CkaClass.ToString()),
+                    new Markup(Markup.Escape(specificObject.Description)));
+            }
+
+            AnsiConsole.Write(table);
+            AnsiConsole.WriteLine();
+        }
+
         return 0;
     }
 }
