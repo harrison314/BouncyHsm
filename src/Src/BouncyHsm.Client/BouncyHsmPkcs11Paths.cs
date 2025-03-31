14d67e1b30408dd3d4060584714be21a9b1623df
harrison314 <harrison314@azet.sk>
2025-03-31 18:00:24 +0200

    Add class BouncyHsmPkcs11Paths with native path resolving into client.

---
+++ src/Src/BouncyHsm.Client/BouncyHsmPkcs11Paths.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+namespace BouncyHsm.Client;
+
+[UnsupportedOSPlatform("browser")]
+public static class BouncyHsmPkcs11Paths
+{
+    public const string WindowsX64 = @"runtimes\win-x64\native\BouncyHsm.Pkcs11Lib.dll";
+    public const string WindowsX86 = @"runtimes\win-x86\native\BouncyHsm.Pkcs11Lib.dll";
+    public const string LinuxX64 = @"runtimes/linux-x64/native/BouncyHsm.Pkcs11Lib.so";
+
+    public static string CurrentPlatformSpecificPkcs11Path
+    {
+        get => GetPlatformPkcs11Path();
+    }
+
+    private static string GetPlatformPkcs11Path()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            if (Environment.Is64BitProcess)
+            {
+                return WindowsX64;
+            }
+            else
+            {
+                return WindowsX86;
+            }
+
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && Environment.Is64BitProcess)
+        {
+            return LinuxX64;
+        }
+
+#if NET8_0_OR_GREATER
+        throw new PlatformNotSupportedException($"Platfrom {RuntimeInformation.RuntimeIdentifier} is not supported by the native library.");
+#else
+        throw new PlatformNotSupportedException();
+#endif
+    }
+}
+
---
+++ src/Src/BouncyHsm.Client/NetPolyFill.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if NETSTANDARD2_1
+namespace System.Runtime.Versioning
+{
+    internal abstract class OSPlatformAttribute : Attribute
+    {
+        public string PlatformName { get; }
+        private protected OSPlatformAttribute(string platformName)
+        {
+            this.PlatformName = platformName;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Assembly |
+                    AttributeTargets.Class |
+                    AttributeTargets.Constructor |
+                    AttributeTargets.Enum |
+                    AttributeTargets.Event |
+                    AttributeTargets.Field |
+                    AttributeTargets.Interface |
+                    AttributeTargets.Method |
+                    AttributeTargets.Module |
+                    AttributeTargets.Property |
+                    AttributeTargets.Struct,
+                    AllowMultiple = true, Inherited = false)]
+    internal sealed class UnsupportedOSPlatformAttribute : OSPlatformAttribute
+    {
+        public string? Message { get; }
+
+        public UnsupportedOSPlatformAttribute(string platformName) : base(platformName)
+        {
+        }
+
+        public UnsupportedOSPlatformAttribute(string platformName, string? message) : base(platformName)
+        {
+            this.Message = message;
+        }
+    }
+}
+
+#endif
