a677d94d8e4e3e0b88442104c6045d2e88a62b80
harrison314 <harrison314@azet.sk>
2025-03-31 19:12:40 +0200

    #25 Update BouncyHsmPkcs11Paths in client.

--- src/Src/BouncyHsm.Client/BouncyHsmPkcs11Paths.cs
+++ src/Src/BouncyHsm.Client/BouncyHsmPkcs11Paths.cs
@@ -9,7 +9,11 @@ public static class BouncyHsmPkcs11Paths
     public const string WindowsX64 = @"runtimes\win-x64\native\BouncyHsm.Pkcs11Lib.dll";
     public const string WindowsX86 = @"runtimes\win-x86\native\BouncyHsm.Pkcs11Lib.dll";
     public const string LinuxX64 = @"runtimes/linux-x64/native/BouncyHsm.Pkcs11Lib.so";
+    public const string RhelX64 = @"runtimes/rhel-x64/native/BouncyHsm.Pkcs11Lib.so";
 
+#if !NET8_0_OR_GREATER
+    [Obsolete("For the .netstandard2.1 this property does not give correct results for RHEL like distributions.")]
+#endif
     public static string CurrentPlatformSpecificPkcs11Path
     {
         get => GetPlatformPkcs11Path();
@@ -32,6 +36,14 @@ public static class BouncyHsmPkcs11Paths
 
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && Environment.Is64BitProcess)
         {
+#if NET8_0_OR_GREATER
+            string rid = RuntimeInformation.RuntimeIdentifier;
+            if (IsRhelLike(rid))
+            {
+                return RhelX64;
+            }
+#endif
+
             return LinuxX64;
         }
 
@@ -41,5 +53,21 @@ public static class BouncyHsmPkcs11Paths
         throw new PlatformNotSupportedException();
 #endif
     }
-}
 
+    private static bool IsRuntimeIdentifierPrefix(string rid, string prefix)
+    {
+        return string.Equals(prefix, rid, StringComparison.OrdinalIgnoreCase)
+            || rid.StartsWith(string.Concat(prefix, "."), StringComparison.OrdinalIgnoreCase)
+            || rid.StartsWith(string.Concat(prefix, "-"), StringComparison.OrdinalIgnoreCase);
+    }
+
+    // See: https://github.com/dotnet/runtime/blob/main/src/libraries/Microsoft.NETCore.Platforms/src/runtime.json
+    private static bool IsRhelLike(string rid)
+    {
+        return IsRuntimeIdentifierPrefix(rid, "rhel")
+            || IsRuntimeIdentifierPrefix(rid, "rocky")
+            || IsRuntimeIdentifierPrefix(rid, "ol")
+            || IsRuntimeIdentifierPrefix(rid, "miraclelinux")
+            || IsRuntimeIdentifierPrefix(rid, "centos");
+    }
+}
