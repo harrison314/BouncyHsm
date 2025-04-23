b6cb675511e4c7a9192906b0d094acea526f7b70
harrison314 <harrison314@azet.sk>
2025-04-23 19:14:04 +0200

    #34 Implement Wrap/Unwrap for CKM_AES_CBC

---
+++ src/Src/BouncyHsm.Core/Services/Bc/Asn1ObjectParser.cs
@@ -0,0 +1,39 @@
+﻿using Org.BouncyCastle.Asn1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BouncyHsm.Core.Services.Bc;
+
+internal static class Asn1ObjectParser
+{
+    public static Asn1Object FromByteArray(byte[] data, bool accetExtraData)
+    {
+        System.Diagnostics.Debug.Assert(data != null);
+
+        try
+        {
+            int limit = data.Length;
+            using MemoryStream ms = new MemoryStream(data, false);
+            using Asn1InputStream asn1In = new Asn1InputStream(ms,
+                limit,
+                leaveOpen: true);
+
+            Asn1Object result = asn1In.ReadObject();
+
+            if (!accetExtraData && asn1In.Position != limit)
+            {
+                throw new IOException("extra data found after object");
+            }
+
+            return result;
+
+        }
+        catch (InvalidCastException)
+        {
+            throw new IOException("cannot recognise object in byte array");
+        }
+    }
+}
