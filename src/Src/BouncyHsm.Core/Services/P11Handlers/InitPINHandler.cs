eb8a312620bb3ca96d9937c931556313016c95f8
harrison314 <harrison314@azet.sk>
2025-07-29 19:18:34 +0200

    #49 Implementation InitTokenHandler

---
+++ src/Src/BouncyHsm.Core/Services/P11Handlers/InitPINHandler.cs
@@ -0,0 +1,15 @@
+﻿using BouncyHsm.Core.Rpc;
+
+namespace BouncyHsm.Core.Services.P11Handlers;
+
+public partial class InitPINHandler : IRpcRequestHandler<InitPinRequest, InitPinEnvelope>
+{
+    public InitPINHandler()
+    {
+    }
+
+    public ValueTask<InitPinEnvelope> Handle(InitPinRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException("InitTokenHandler is not implemented yet.");
+    }
+}
---
+++ src/Src/BouncyHsm.Core/UseCases/Implementation/SlotCommands/ChangeLabelCommand.cs
@@ -0,0 +1,20 @@
+﻿using BouncyHsm.Core.Services.Contracts;
+using BouncyHsm.Core.Services.Contracts.Entities;
+
+namespace BouncyHsm.Core.UseCases.Implementation.SlotCommands;
+
+internal class ChangeLabelCommand : IPersistentRepositorySlotCommand
+{
+    private readonly string newLabel;
+
+    public ChangeLabelCommand(string newLabel)
+    {
+        this.newLabel = newLabel;
+    }
+
+    public bool UpdateSlot(SlotEntity slotEntity)
+    {
+        slotEntity.Token.Label = this.newLabel.Trim();
+        return true;
+    }
+}
