0ffb4004241f7b884edd047aebe56d0dffcb18ce
harrison314 <harrison314@azet.sk>
2026-04-29 17:51:14 +0200

    #91 Make MemoryPersistentRepository thread safe.

---
+++ src/Src/BouncyHsm.Infrastructure/Common/StringOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BouncyHsm.Infrastructure.Common;
+
+internal static class StringOperations
+{
+    [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+    public static bool FixedTimeEquals(ReadOnlySpan<char> a, ReadOnlySpan<char> b)
+    {
+        if (a.Length != b.Length)
+        {
+            return false;
+        }
+
+        bool result = true;
+        for (int i = 0; i < a.Length; i++)
+        {
+            result &= a[i] == b[i];
+        }
+
+        return result;
+    }
+}
