9a4cedd7725a89dd2c75baf138991bcb8e8470bf
harrison314 <harrison314@azet.sk>
2026-05-01 12:03:48 +0200

    Add null check into BouncyHsmClient

--- src/Src/BouncyHsm.Client/BouncyHsmClient.cs
+++ src/Src/BouncyHsm.Client/BouncyHsmClient.cs
@@ -5,6 +5,9 @@ public partial class BouncyHsmClient
     public BouncyHsmClient(string baseAddress, HttpClient httpClient)
         : this(httpClient)
     {
+        if (baseAddress == null) throw new ArgumentNullException(nameof(baseAddress));
+        if (httpClient == null) throw new ArgumentNullException(nameof(httpClient));
+
         this._baseUrl = baseAddress;
     }
 }
