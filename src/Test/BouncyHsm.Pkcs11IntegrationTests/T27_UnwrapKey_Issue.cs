bf9e1337f51a58284b47921a073e67ec38f92a82
harrison314 <harrison314@azet.sk>
2026-06-03 17:04:29 +0200

    #92 Create test for replication issue

---
+++ src/Test/BouncyHsm.Pkcs11IntegrationTests/T27_UnwrapKey_Issue.cs
@@ -0,0 +1,111 @@
+﻿using Net.Pkcs11Interop.Common;
+using Net.Pkcs11Interop.HighLevelAPI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BouncyHsm.Pkcs11IntegrationTests;
+
+[TestClass]
+public sealed class T27_UnwrapKey_Issue
+{
+    [TestMethod]
+    [DataRow(CKM.CKM_AES_CBC_PAD)]
+    public void Unwrap_AesWithIv_SuccessFromNet(CKM mechanismType)
+    {
+        using RSA rsa = RSA.Create(4096);
+        byte[] pkcs8PrivateKey = rsa.ExportPkcs8PrivateKey();
+
+        using Aes aes = Aes.Create();
+        aes.KeySize = 256;
+        aes.Mode = CipherMode.CBC;
+        aes.Padding = PaddingMode.PKCS7;
+
+        aes.GenerateKey();
+        aes.GenerateIV();
+
+        byte[] aesKey = aes.Key;
+        byte[] iv = aes.IV;
+
+        byte[] encryptedPkcs8;
+        using (ICryptoTransform encryptor = aes.CreateEncryptor(aesKey, iv))
+        {
+            encryptedPkcs8 = encryptor.TransformFinalBlock(pkcs8PrivateKey, 0, pkcs8PrivateKey.Length);
+        }
+
+        Pkcs11InteropFactories factories = new Pkcs11InteropFactories();
+        using IPkcs11Library library = factories.Pkcs11LibraryFactory.LoadPkcs11Library(factories,
+            AssemblyTestConstants.P11LibPath,
+            AppType.SingleThreaded);
+
+        List<ISlot> slots = library.GetSlotList(SlotsType.WithTokenPresent);
+        ISlot slot = slots.SelectTestSlot();
+
+        using ISession session = slot.OpenSession(SessionType.ReadWrite);
+        session.Login(CKU.CKU_USER, AssemblyTestConstants.UserPin);
+
+
+        IObjectHandle key = this.CreateAesKey(session, aesKey);
+
+        using IMechanism mechanism = session.Factories.MechanismFactory.Create(mechanismType, iv);
+
+        IObjectHandle unwrappedKey = session.UnwrapKey(mechanism, key, encryptedPkcs8, this.GetPrivateRsaKeyTemplate(session));
+    }
+
+    private IObjectHandle CreateAesKey(ISession session, byte[] secret)
+    {
+        string label = $"Aes-{DateTime.UtcNow}-{Random.Shared.Next(100, 999)}";
+        byte[] ckId = session.GenerateRandom(32);
+
+
+        List<IObjectAttribute> objectAttributes = new List<IObjectAttribute>()
+        {
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_CLASS, CKO.CKO_SECRET_KEY),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_KEY_TYPE, CKK.CKK_AES),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_TOKEN, false),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_PRIVATE, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_COPYABLE, false),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_SENSITIVE, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_EXTRACTABLE, false),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_DESTROYABLE, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_ID, ckId),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_LABEL, label),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_ENCRYPT, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_DECRYPT, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_SIGN, false),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_VERIFY, false),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_DERIVE, false),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_WRAP, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_UNWRAP, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_VALUE, secret),
+        };
+
+        return session.CreateObject(objectAttributes);
+    }
+
+    private List<IObjectAttribute> GetPrivateRsaKeyTemplate(ISession session)
+    {
+        string label = $"RSAKeyUn-{DateTime.UtcNow}-{RandomNumberGenerator.GetInt32(100, 999)}";
+        byte[] ckId = session.GenerateRandom(32);
+
+        List<IObjectAttribute> privateKeyAttributes = new List<IObjectAttribute>()
+        {
+             session.Factories.ObjectAttributeFactory.Create(CKA.CKA_CLASS, CKO.CKO_PRIVATE_KEY),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_KEY_TYPE, CKK.CKK_RSA),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_TOKEN, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_PRIVATE, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_LABEL, label),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_ID, ckId),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_SENSITIVE, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_EXTRACTABLE, true),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_DECRYPT, false),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_SIGN, false),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_SIGN_RECOVER, false),
+            session.Factories.ObjectAttributeFactory.Create(CKA.CKA_UNWRAP, true)
+        };
+
+        return privateKeyAttributes;
+    }
+}
