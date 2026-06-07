2439404ace569c380fa069ba9b5cf78625d5e0cc
harrison314 <harrison314@azet.sk>
2026-06-07 19:47:59 +0200

    Fix redefined STRICT macro.

--- src/Src/BouncyHsm.Pkcs11Lib/includeWindows.h
+++ src/Src/BouncyHsm.Pkcs11Lib/includeWindows.h
@@ -1,12 +1,27 @@
 #ifndef INCLUDE_WINDOWS_H
 #define INCLUDE_WINDOWS_H
 
+#ifndef VC_EXTRALEAN
 #define VC_EXTRALEAN
+#endif
+#ifndef STRICT
 #define STRICT
+#endif
+#ifndef STRICT
+#define STRICT
+#endif
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
+#ifndef NOGDI
 #define NOGDI
+#endif
+#ifndef NOSOUND
 #define NOSOUND
+#endif
+#ifndef NOCOMM
 #define NOCOMM
+#endif
 
 #include <Windows.h>
 
