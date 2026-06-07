ee6e8b0a4fa2bd7d20fe318611d511daaa9b2cdc
harrison314 <harrison314@azet.sk>
2026-06-07 13:43:12 +0200

    #78 Add test for write/readt template attributes.

--- src/Test/BouncyHsm.Pkcs11IntegrationTests/T28_SetAttributeValue.cs
+++ src/Test/BouncyHsm.Pkcs11IntegrationTests/T28_SetAttributeValue.cs
@@ -310,4 +310,110 @@ public class T28_SetAttributeValue
 
         session.SetAttributeValue(privateKey, template);
     }
+
+    [TestMethod]
+    [DataRow(0, DisplayName = "With empty template")]
+    [DataRow(1)]
+    [DataRow(2)]
+    [DataRow(3)]
+    [DataRow(4)]
+    [DataRow(5)]
+    [DataRow(6)]
+    [DataRow(7)]
+    [DataRow(8)]
+    public void SetAttributeValue_EcKeyTemplates_Success(int depth)
+    {
+        byte[] namedCurveOid = PkcsExtensions.HexConvertor.GetBytes("06082A8648CE3D030107");
+
+        Pkcs11InteropFactories factories = new Pkcs11InteropFactories();
+        using IPkcs11Library library = factories.Pkcs11LibraryFactory.LoadPkcs11Library(factories,
+            AssemblyTestConstants.P11LibPath,
+            AppType.SingleThreaded);
+
+        List<ISlot> slots = library.GetSlotList(SlotsType.WithTokenPresent);
+        ISlot slot = slots.SelectTestSlot();
+
+        using ISession session = slot.OpenSession(SessionType.ReadWrite);
+        session.Login(CKU.CKU_USER, AssemblyTestConstants.UserPin);
+
+        string label = $"ECKeyTest-{DateTime.UtcNow}-{RandomNumberGenerator.GetInt32(100, 999)}";
+        byte[] ckId = session.GenerateRandom(32);
+
+        List<IObjectAttribute> publicKeyAttributes = new List<IObjectAttribute>()
+        {
+            factories.ObjectAttributeFactory.Create(CKA.CKA_TOKEN, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_PRIVATE, false),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_LABEL, label),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_ID, ckId),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_ENCRYPT, false),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_VERIFY, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_VERIFY_RECOVER, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_WRAP, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_EC_PARAMS, namedCurveOid),
+        };
+
+        List<IObjectAttribute> privateKeyAttributes = new List<IObjectAttribute>()
+        {
+            factories.ObjectAttributeFactory.Create(CKA.CKA_TOKEN, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_PRIVATE, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_LABEL, label),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_ID, ckId),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_SENSITIVE, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_EXTRACTABLE, false),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_DECRYPT, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_SIGN, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_SIGN_RECOVER, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_UNWRAP, true)
+        };
+
+        using IMechanism mechanism = factories.MechanismFactory.Create(CKM.CKM_ECDSA_KEY_PAIR_GEN);
+
+        session.GenerateKeyPair(mechanism,
+            publicKeyAttributes,
+            privateKeyAttributes,
+            out IObjectHandle publicKey,
+            out IObjectHandle privateKey);
+
+        try
+        {
+            List<IObjectAttribute> template = new List<IObjectAttribute>()
+            {
+                factories.ObjectAttributeFactory.Create(CKA.CKA_SIGN, false),
+                factories.ObjectAttributeFactory.Create(CKA.CKA_SIGN_RECOVER, false),
+                factories.ObjectAttributeFactory.Create(CKA.CKA_UNWRAP_TEMPLATE, this.CreateTemplate(factories, depth)),
+            };
+
+            session.SetAttributeValue(privateKey, template);
+        }
+        finally
+        {
+            session.DestroyObject(publicKey);
+            session.DestroyObject(privateKey);
+        }
+    }
+
+    private List<IObjectAttribute> CreateTemplate(Pkcs11InteropFactories factories, int depth)
+    {
+        if (depth == 0)
+        {
+            return new List<IObjectAttribute>();
+        }
+
+        List<IObjectAttribute> attributes = new List<IObjectAttribute>()
+        {
+            factories.ObjectAttributeFactory.Create(CKA.CKA_TOKEN, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_SENSITIVE, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_EXTRACTABLE, false),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_DECRYPT, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_SIGN, true),
+            factories.ObjectAttributeFactory.Create(CKA.CKA_URL, "http://test.eu/"),
+        };
+
+        if (depth > 1)
+        {
+            factories.ObjectAttributeFactory.Create(CKA.CKA_UNWRAP_TEMPLATE, this.CreateTemplate(factories, depth - 1));
+        }
+
+        return attributes;
+    }
 }
