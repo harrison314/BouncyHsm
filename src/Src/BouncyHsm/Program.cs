c146dc191f50b302e138ab95d7c03214c4825198
harrison314 <harrison314@azet.sk>
2026-09-18 18:57:02 +0200

    Update version to 2.3.1

--- src/Src/BouncyHsm/Program.cs
+++ src/Src/BouncyHsm/Program.cs
@@ -60,7 +60,7 @@ public class Program
             cfg.Title = "Bouncy Hsm REST API";
             cfg.Description = "Management API for Bouncy Hsm.";
             cfg.UseRouteNameAsOperationId = true;
-            cfg.Version = "2.3.1";
+            cfg.Version = "2.3.2";
         });
 
         builder.Host.UseWindowsService();
